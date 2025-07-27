f59579a568dbc110ebedea7cae232df6ef0fff82
Alok Singh <AlokSingh1200@gmail.com>
2025-07-27 20:31:27 +0530

    work in cppstl

---
+++ stl.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+void explanPoint() {
+    pair<int, int> p = {1, 3};
+    cout << p.first << " " << p.second << endl;
+}
+
+int main() {
+    explanPoint();
+    return 0;
+}
