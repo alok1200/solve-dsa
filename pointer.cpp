8aec40d0632cf05af52d544168d73e98d394f859
Alok Singh <salok2857@gmail.com>
2025-09-25 14:21:32 +0530

    solve small quistion in using pointer

--- pointer.cpp
+++ pointer.cpp
@@ -6,10 +6,29 @@ using namespace std;
 int main(){
     // int a = 10;
     // int* ptr = &a;
-    int a = 10;
-    int* ptr = &a;
+    // int a = 10;
+    // int* ptr = &a;
+    // cout << *(&a) << endl;
+    // cout << *(ptr) << endl;
 
-    cout << *(&a) << endl;
-    cout << *(ptr) << endl;
+    //-----------------------------------
+    //Null Pointer              thsy always stor garbage value that we use Null value
+    // int** ptr = NULL;
+
+    // cout << ptr << endl; 
+    // return 0;
+
+    //-----------------------------------
+    // solve quistion of pointer
+
+    int a = 5;
+    int *p = &a;
+    int **q = &p;
+
+    cout << *p << endl;
+    cout << **q << endl;
+    cout << &a << endl;
+    cout << *q << endl;
+
     return 0;
 }
