933c2f5aa63ba2ebefebbebaf1ddfa74cdea35ce
Alok Singh <salok2857@gmail.com>
2025-09-26 20:49:38 +0530

    this is how we find the adderss and value

--- pointer.cpp
+++ pointer.cpp
@@ -21,14 +21,35 @@ int main(){
     //-----------------------------------
     // solve quistion of pointer
 
-    int a = 5;
-    int *p = &a;
-    int **q = &p;
+    // int a = 5;
+    // int *p = &a;
+    // int **q = &p;
 
-    cout << *p << endl;
-    cout << **q << endl;
-    cout << &a << endl;
-    cout << *q << endl;
+    // cout << *p << endl;
+    // cout << **q << endl;
+    // cout << &a << endl;
+    // cout << *q << endl;
+    //-----------------------------------
+    // pass by reference
+
+    int arr [5] = {1,2,3,4,5};
+    int *ptr = arr;
+
+    cout << arr << endl;
+    cout << *arr << endl;
+    cout << &ptr[0] << endl;
+    cout << ptr << endl;
+    cout << ptr + 1 << endl;
+    cout << &ptr[1] << endl;
+
+
+    for(int i = 0; i<5; i++){
+        cout << arr+i << endl;
+    }
+
+    for(int i = 0; i<5; i++){
+        cout << *(arr+i) << endl;
+    }
 
     return 0;
 }
