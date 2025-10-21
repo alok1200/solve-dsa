c9088353befca640ab01be8dc31266e760925712
Alok Singh <salok2857@gmail.com>
2025-10-21 10:22:00 +0530

    this for taking input

--- selectionshort.cpp
+++ selectionshort.cpp
@@ -1,24 +1,32 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
-    int arr[5] = {4, 5, 6, 3, 2};
+    int arr[1000];
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    cout << "Enter the elements of the array: ";
 
-    for (int i = 0; i < 5; i++)
-    {
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+
+    for (int i = 0; i < n - 1; i++) {
         int index = i;
-        for (int j = i + 1; j < 5; j++)
-        {
+        for (int j = i + 1; j < n; j++) { 
             if (arr[j] < arr[index])
                 index = j;
         }
         swap(arr[i], arr[index]);
     }
 
-    for (int i = 0; i < 5; i++)
-    {
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) { 
         cout << arr[i] << " ";
     }
+
+    return 0;
 }
