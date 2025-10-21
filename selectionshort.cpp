76b2fc04b5d34304b33d0dc86c4b53c9d07f91c8
Alok Singh <salok2857@gmail.com>
2025-10-21 10:38:43 +0530

    this is hoe we using reverce shorting

--- selectionshort.cpp
+++ selectionshort.cpp
@@ -17,7 +17,7 @@ int main()
     for (int i = 0; i < n - 1; i++) {
         int index = i;
         for (int j = i + 1; j < n; j++) {
-            if (arr[j] > arr[index]) 
+            if (arr[j] > arr[index]) f
                 index = j;
         }
         swap(arr[i], arr[index]);
