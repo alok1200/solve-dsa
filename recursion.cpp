cb1d47efb50fcef259007f62e17348e244ead0f3
Alok Singh <salok2857@gmail.com>
2026-04-12 16:38:31 +0530

    recursio value and providing output

--- recursion.cpp
+++ recursion.cpp
@@ -1,36 +1,83 @@
-#include <iostream>
+// #include <iostream>
 
+// using namespace std;
+
+// int factorial(int n)
+// {
+//     if (n <= 1)
+//     {
+//         return 1;
+//     }
+//     return n * factorial(n - 1);
+// }
+
+// int main()
+// {
+//     // Always show a demo output so you can verify recursion works.
+//     int demo = 5;
+//     cout << "Demo: Factorial of " << demo << " is " << factorial(demo) << endl;
+
+//     int n;
+//     cout << "Enter a number (or press Ctrl+D to skip): ";
+
+//     if (!(cin >> n))
+//     {
+//         cout << "No input given. Demo output shown above." << endl;
+//         return 0;
+//     }
+
+//     if (n < 0)
+//     {
+//         cout << "Factorial is not defined for negative numbers." << endl;
+//         return 0;
+//     }
+
+//     cout << "Factorial of " << n << " is " << factorial(n) << endl;
+// }
+
+#include <iostream>
 using namespace std;
 
-int factorial(int n)
+bool linearSearch(int arr[], int x, int index, int N)
 {
-    if (n <= 1)
+    if (index == N)
     {
-        return 1;
+        return false;
     }
-    return n * factorial(n - 1);
+    if (arr[index] == x)
+    {
+        return true;
+    }
+    return linearSearch(arr, x, index + 1, N);
 }
 
 int main()
 {
-    // Always show a demo output so you can verify recursion works.
-    int demo = 5;
-    cout << "Demo: Factorial of " << demo << " is " << factorial(demo) << endl;
+    int arr[] = {2, 3, 6, 7, 1, 5};
+    int N = sizeof(arr) / sizeof(arr[0]);
 
-    int n;
-    cout << "Enter a number (or press Ctrl+D to skip): ";
+    int x;
+    cout << "Enter value to search: ";
+    cin >> x;
 
-    if (!(cin >> n))
+    bool found = linearSearch(arr, x, 0, N);
+
+    cout << "\nOutput:\n";
+
+    // Print as 0 or 1
+    cout << "Returned (0/1): " << found << endl;
+
+    // Print as true or false
+    cout << "Returned (true/false): " << boolalpha << found << endl;
+
+    if (found)
     {
-        cout << "No input given. Demo output shown above." << endl;
-        return 0;
+        cout << "Value " << x << " is in the array." << endl;
+    }
+    else
+    {
+        cout << "Value " << x << " is not in the array." << endl;
     }
 
-    if (n < 0)
-    {
-        cout << "Factorial is not defined for negative numbers." << endl;
-        return 0;
-    }
-
-    cout << "Factorial of " << n << " is " << factorial(n) << endl;
+    return 0;
 }
